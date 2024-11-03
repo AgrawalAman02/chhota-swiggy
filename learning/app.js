f4a1d1af93d92cc97ab80cb94461395dd0d237c8
AgrawalAman02 <amanhzb2003@gmail.com>
2024-11-03 13:06:36 +0530

    Enhance React learning

--- learning/app.js
+++ learning/app.js
@@ -1,27 +1,64 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// const heading = React.createElement("h1",{},"Hello From the React!");    // {} in curly brace we give attributes
-const heading = React.createElement(
-    "h1",
-    {id:"heading"},
-    "Hello From the React!"
-);
+// using JSX = Not HTML in JS but is HTML like syntax in JS
 
-// console.log(heading); // so what is this heading? it is a object of js where props contains the attributes and type contains the type of element and key is null
+const jsxHeading = (<h1 id="heading" className="Head"> Hello From the jsx heading </h1>);  // if it is single line we dont need () but if it is multiline then we need to use ()
+console.log(jsxHeading); // it is a object of js where props contains the attributes and type contains the type of element and key is null
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);
-const parent = React.createElement(
-    "div",
-    {id:"parent"},
-    React.createElement(
-        "div",
-        {id:"child"},
-        [React.createElement("h1", {}, "Hii from the nested h1 . How are u?"), React.createElement("h2", {}, "Hii from the nested h2. Lets do some coding!")]
-    )
-);
+root.render(jsxHeading);
 
-// if we are going to code like this then it will become very complex so we can use JSX for this
-console.log(parent);
-root.render(parent);
+// REact component
+// Class based component  - OLD way - nobody uses now
+// functional component  - NEW - It is just a normal js function that returns JSX code - ALways name in capital letter 
+
+const HeadingComponent = () =>{
+    return <h1>Namaste React from component</h1>
+};
+// or we can use shorthand function if we are returning only  so we dont need to write return and {}
+
+const HeadingComponent1 = () => <h1>Namaste React from component 1</h1>; 
+// if it contains multiple line then we wrap it in () 
+// we can even use shorthand for nested elements like above 
+
+// now to render this react component we use </> so that babel can understand
+root.render(<HeadingComponent/>);
+
+// ! Component Composition - combining multiple components to create a new component
+// we can use the component inside the component
+
+// for eg
+const ParentComponent = () => {
+    return (
+        <div>
+            <HeadingComponent/>
+            <HeadingComponent1/>
+        </div>
+    );
+};
+
+root.render(<ParentComponent/>);
+
+
+// we can even write javasript inside component but with curly braces;
+// let see 
+
+const number = 1000;
+
+const ParentComponent2 = () => {
+    return (
+        <div>
+            {number}
+            <h2>{number*3}{console.log(number)}</h2>
+            <HeadingComponent/>
+            <HeadingComponent1/>
+            {HeadingComponent()}  // we can even call the function like this
+        </div>
+    );
+};
+
+root.render(<ParentComponent2/>);
+
+// we can even insert react element like this - {react-element}
+// we can even insert react element inside the react element or react component inside the react element
