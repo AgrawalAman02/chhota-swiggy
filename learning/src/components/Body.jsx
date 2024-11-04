0dd29b4881e9213a6551b47e82105e1197e7e7c8
AgrawalAman02 <amanhzb2003@gmail.com>
2024-11-04 20:28:09 +0530

    Refactor search functionality and add filter button

--- learning/src/components/Body.jsx
+++ learning/src/components/Body.jsx
@@ -1,14 +1,29 @@
 import RestaurantCards from "./RestaurantCards";
+import { useState } from "react";
 import resList from "../utils/mockData";
 
 const Body=()=>{
+
+    const [listOfRestaurants , setListOfRetaurants] = useState(resList);
+
     return (
         <div className="body">
-            <div  className ="search"> Search </div>
+            <div  className ="filter"> 
+                <button 
+                className="filter-btn"
+                onClick={()=>{
+                    const filteredList = listOfRestaurants.filter(
+                        (res)=> res.card.card.info.avgRating>=4.5
+                    );
+                    setListOfRetaurants(filteredList);
+                }}
+                >Top Rated Restaurants</button>
+
+            </div>
             <div className="res-container">
 
                 {
-                    resList.map((restaurant) => {
+                    listOfRestaurants.map((restaurant) => {
                         return (
                           <RestaurantCards key={restaurant.card.card.info.id} resData={restaurant} />
                         );
