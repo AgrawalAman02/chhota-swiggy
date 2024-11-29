44385382c3e51de02e8057a84ae2a9243e364623
AgrawalAman02 <amanhzb2003@gmail.com>
2024-11-29 19:34:32 +0530

    Prepare for Vercel deployment

---
+++ api/cors.js
@@ -0,0 +1,10 @@
+
+const cors_proxy = require('cors-anywhere');
+
+module.exports = (req, res) => {
+  cors_proxy.createServer({
+    originWhitelist: [], // Allow all origins
+    requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: ['cookie', 'cookie2']
+  }).emit('request', req, res);
+};
