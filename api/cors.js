4c748bd9b4f2c5e1ceade87e979328ffffa5072d
AgrawalAman02 <amanhzb2003@gmail.com>
2024-11-29 20:48:52 +0530

    issues

--- api/cors.js
+++ api/cors.js
@@ -1,10 +1,13 @@
+const CorsAnywhere = require('cors-anywhere');
 
-const cors_proxy = require('cors-anywhere');
+// Initialize the server once
+const server = CorsAnywhere.createServer({
+  originWhitelist: [], // Allow all origins
+  requireHeader: ['origin', 'x-requested-with'],
+  removeHeaders: ['cookie', 'cookie2'],
+  // Additional configurations if necessary
+});
 
 module.exports = (req, res) => {
-  cors_proxy.createServer({
-    originWhitelist: [], // Allow all origins
-    requireHeader: ['origin', 'x-requested-with'],
-    removeHeaders: ['cookie', 'cookie2']
-  }).emit('request', req, res);
+  server.emit('request', req, res);
 };
